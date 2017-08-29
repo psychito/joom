865e41e105917f92f0abccebdbc086f17cef2e9b
drPsychito <alexanderurbinac@gmail.com>
2017-08-29 17:19:14 -0500

    base funcional con fecha en español

---
+++ administrator/logs/jcontroller.log.php
@@ -0,0 +1,11 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2017-08-29 21:57:50 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2017-08-29T21:57:50+00:00	INFO ::1	controller	Holding edit ID com_modules.edit.module.89 Array (     [0] => 89 ) 
+2017-08-29T21:57:50+00:00	INFO ::1	controller	Checking edit ID com_modules.edit.module.89: 1 Array (     [0] => 89 ) 
+2017-08-29T22:02:27+00:00	INFO ::1	controller	Checking edit ID com_modules.edit.module.89: 1 Array (     [0] => 89 ) 
+2017-08-29T22:03:37+00:00	INFO ::1	controller	Checking edit ID com_modules.edit.module.89: 1 Array (     [0] => 89 ) 
+2017-08-29T22:13:12+00:00	INFO ::1	controller	Releasing edit ID com_modules.edit.module.89 Array ( ) 
